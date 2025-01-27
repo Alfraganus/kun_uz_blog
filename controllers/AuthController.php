dea9c481c3eb826ba9c54b83654d0cd4d2b1a8e5
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2025-01-27 12:20:41 +0500

    kichik bugfix

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -2,17 +2,12 @@
 
 namespace app\controllers;
 
-use app\models\CompanyProfile;
-use app\models\User;
-use Carbon\Carbon;
 use Yii;
-use yii\base\Model;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
-use app\models\ContactForm;
 
 class AuthController extends Controller
 {
@@ -69,40 +64,6 @@ class AuthController extends Controller
     {
         return $this->render('index');
     }
-
-
-    public function actionRegister()
-    {
-        $userModel = new User();
-        $userProfile = new CompanyProfile();
-          if ($userModel->load(Yii::$app->request->post()) && $userProfile->load(Yii::$app->request->post()))
-           {
-              $transaction = Yii::$app->getDb()->beginTransaction();
-            try {
-                if ($user = $userModel->signup($userModel->password_hash)) {
-                    $role = Yii::$app->authManager->getRole('cadmin');
-                    Yii::$app->authManager->assign($role,$user->id);
-                }
-                if($userProfile->free_trial == 1)  {
-                    $userProfile->free_triel_start_date =Carbon::now();
-                    $userProfile->free_triel_end_date =(new Carbon())::now()->addDays(15);
-                }
-                $userProfile->user_id = $user->id;
-                $userProfile->save();
-
-                $transaction->commit();
-                return $this->redirect(['auth/login']);
-            }catch (DomainException $exception){
-                $transaction->rollBack();
-                Yii::$app->session->setFlash('error', $exception->getMessage());
-            }
-        }
-        return $this->render('register', [
-            'userModel' => new User(),
-            'userProfile' =>new CompanyProfile(),
-        ]);
-    }
-
 
     public function actionLogin()
     {
